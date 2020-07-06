38e859bc8a845cee78fc3f1c285013f8d39c08dc
CFWarfare <67923871+CFWarfare@users.noreply.github.com>
2020-07-06 15:37:36 -0300

    ye

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,34 @@
+const moment = require('moment')
+const Discord = require('discord.js')
+
+/**
+ * O evento guildMemberAdd é emitido após um membro entrar (ser adicionado em uma guild).
+ */
+
+module.exports = async (client, member) => {
+  // Verificações anti-selfbot de divulgação já que estamos tendo problemas com isso.
+  const daysSinceCreation = moment().diff(moment(member.user.createdAt), 'days')
+  const isDefaultAvatar = member.user.displayAvatarURL.startsWith('https://discordapp.com/')
+  const domaincount = member.user.username.match(/\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/)
+  if (domaincount > 0 && (isDefaultAvatar || daysSinceCreation < 3)) return (() => { member.send('Olá! você foi kickado automaticamente por suspeita de divulgação em nosso servidor. Contas com menos de 3 dias no discord não podem ter domínios (exemplo twitter.com)').catch(); member.kick('Autokick: Selfbots não são bem vindos').catch() })()
+
+  const message = new Discord.RichEmbed()
+    .setThumbnail(member.user.displayAvatarURL)
+    .setColor('RANDOM')
+    .setAuthor('👋 Bem-vindo(a) a Media Bellum!')
+    .setTitle(`${member} entrou pela primeira vez!`)
+    .setDescription(`${member}, vá para <#729509767713652786> e se apresente!.`)
+    .setFooter('2D - 2020')
+    .setTimestamp()
+
+  const join = new Discord.RichEmbed()
+    .setThumbnail(member.user.displayAvatarURL)
+    .setColor('RANDOM')
+    .setAuthor('✨ Um novo membro entrou no servidor!')
+    .setDescription(`${member} acabou de entrar.`)
+    .setFooter('2D - 2020')
+    .setTimestamp()
+
+  member.guild.channels.get(process.env.JOINCHANNEL).send(join).catch()
+  member.guild.channels.get(process.env.GREETCHANNEL).send(message).catch()
+}
